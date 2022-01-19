d475dff3c851cec06d6b046e10645843fae022d3
nicccoo <nicolas_31_07@hotmail.com>
2022-01-19 01:48:37 -0300

    fix: router imlpemented

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <h1>LOGO</h1>
+        </div>
+    )
+}
+
+export default Header
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzvGv32DS5_YXft_8vk7im3zWVHx04PaU",
-  authDomain: "ecommerce-react-9d2e7.firebaseapp.com",
-  projectId: "ecommerce-react-9d2e7",
-  storageBucket: "ecommerce-react-9d2e7.appspot.com",
-  messagingSenderId: "191083484146",
-  appId: "1:191083484146:web:f8517716fca9576f9fd16b",
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
