0a6dd9169d95c538c977d70fc5a91da04ee81524
nicccoo <nicolas_31_07@hotmail.com>
2022-01-19 01:49:08 -0300

    feat: add links and some styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styles from './styles.module.css';
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <h1>LOGO</h1>
+                <input type="text" /> 
+            <div className={styles.navbar}>
+                <ul className={styles.navbarul}>
+                <NavLink className={styles.navlink} to="/">Home</NavLink>
+                <NavLink className={styles.navlink} to="/">Products</NavLink>
+                <NavLink className={styles.navlink} to="/">Cart</NavLink>
+               </ul>
+            </div>
         </div>
     )
 }
