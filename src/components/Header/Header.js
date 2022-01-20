90cc1f442f265fa13a326f95151f260cafae5346
nicccoo <nicolas_31_07@hotmail.com>
2022-01-19 23:51:24 -0300

    feat: new navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './styles.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./styles.module.css";
 
 const Header = () => {
-    return (
-        <div className={styles.header}>
-            <h1>LOGO</h1>
-                <input type="text" /> 
-            <div className={styles.navbar}>
-                <ul className={styles.navbarul}>
-                <NavLink className={styles.navlink} to="/">Home</NavLink>
-                <NavLink className={styles.navlink} to="/">Products</NavLink>
-                <NavLink className={styles.navlink} to="/">Cart</NavLink>
-               </ul>
-            </div>
+  return (
+    <div className={styles.header}>
+      <div className={styles.navbar}>
+      <h1>LOGO</h1>
+        <ul className={styles.center}>
+          <NavLink className={styles.navlink} to="/">
+            Home
+          </NavLink>
+          <NavLink className={styles.navlink} to="/products">
+            Products
+          </NavLink>
+          <NavLink className={styles.navlink} to="/contact">
+            Contacto
+          </NavLink>
+        </ul>
+        <div className={styles.right}>
+          <input type="text" />
+          <h3>cart</h3>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
