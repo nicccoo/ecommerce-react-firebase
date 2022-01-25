a62098d06b07b1135496e8a965035e31d0e6fa54
nicccoo <nicolas_31_07@hotmail.com>
2022-01-25 12:33:43 -0300

    feat: new product component

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import db from "../../firebase/firebaseConfig";
+import React from "react";
+import styles from "./styles.module.css";
 
-const Product = () => {
-  const [products, setProducts] = useState([]);
-
-  const getData = async () => {
-    const newProducts = await getDocs(collection(db, "products"));
-    const prod = [];
-    newProducts.forEach((doc) => {
-      prod.push({ ...doc.data(), id: doc.id });
-      setProducts(prod);
-    });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
+const Product = ({ products }) => {
   return (
-    <>
-      {products.map((product) => {
-        return (
-          <div key={product.id}>
-            <h1>{product.title}</h1>
-            <h1>{product.category}</h1>
-            <h1>{product.price}</h1>
+    <div className={styles.container}>
+      {products.map((product) => (
+        <div key={product.id} className={styles.card}>
+          <div className={styles.card_header}>
+            <p>{product.title}</p>
           </div>
-        );
-      })}
-    </>
+          <div className={styles.card_center}>
+            <img src="" alt="" />
+          </div>
+          <div className={styles.card_footer}>
+            <p>{product.category}</p>
+            <p>${product.price}</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
