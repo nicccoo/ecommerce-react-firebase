9cc96a855ae0d1990fb1e4b3143f95d3b8ad8a2b
nicccoo <nicolas_31_07@hotmail.com>
2022-01-25 12:35:18 -0300

    feat: add product component to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Products from "../components/Products/Products";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Products />
     </>
   );
 };
