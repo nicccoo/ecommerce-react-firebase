fc2e8e6b5b738a87dda1a61ba78a216f465c4871
nicccoo <nicolas_31_07@hotmail.com>
2022-01-26 00:15:11 -0300

    feat: new product page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import Products from "../components/Products/Products";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Products />
+      <Header />      
     </>
   );
 };
---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Products from "../components/Products/Products";
+
+const Products = () => {
+  return;
+  <>
+    <Products />
+  </>;
+};
+
+export default Products;
