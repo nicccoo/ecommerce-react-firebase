cef4f501144ab7b5ca98d9631df126421fbadbb0
nicccoo <nicolas_31_07@hotmail.com>
2022-01-26 00:29:46 -0300

    feat: add contact page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import * as ROUTES from './routes/routes'
-import Home from './pages/home'
+import * as ROUTES from "./routes/routes";
+import Home from "./pages/home";
+import Products from "./pages/products";
+import Contact from "./pages/contact";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path={ROUTES.HOME} element={<Home />}/>
+          <Route exact path={ROUTES.HOME} element={<Home />} />
+          <Route exact path={ROUTES.PRODUCTS} element={<Products />} />
+          <Route exact path={ROUTES.CONTACT} element={<Contact />} />
         </Routes>
       </Router>
     </div>
